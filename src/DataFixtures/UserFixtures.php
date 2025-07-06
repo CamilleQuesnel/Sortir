0870fc246f16786ba6cfe2dba282fe74c33d52ab
Sebastien Fischer <113098123+SebastienFischer29@users.noreply.github.com>
2025-07-06 18:47:29 +0200

    commit pause fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -25,36 +25,67 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-
+        $uploadDir = dirname(__DIR__, 2). '/public/upload/images';
+        if (!file_exists($uploadDir)) {
+            mkdir($uploadDir, 0777, true); // true = crée les sous-dossiers si besoin
+        }
         for ($i = 0; $i < 20; $i++) {
             $user = new User();
+            // Appel API randomuser
+            $json = file_get_contents('https://randomuser.me/api/');
+            $data = json_decode($json, true);
+            // Récupération de l'image (grande version)
+            $imageUrl = $data['results'][0]['picture']['large']; // taille large/medium/thumbnail
+
+            // récuperation des infos utiles pour le projet
+            $speudo=$data['results'][0]['login']['username'];
+            $firstName=$data['results'][0]['name']['first'];
+            $lastName=$data['results'][0]['name']['last'];
+            $mail=$data['results'][0]['email'];
+            $phoneNumber=$data['results'][0]['phone'];
+            $image =basename($imageUrl);
+
+            $imageData = file_get_contents($imageUrl);
+            file_put_contents($uploadDir . '/' . $image, $imageData);
+
+
+
             if ($i == 0) {
                 $user->setPseudo('Seb');
                 $user->setFirstName('Sébastien');
                 $user->setLastName('Fischer');
                 $user->setMail('sebastienfischer@hotmail.fr');
                 $user->setPhoneNumber('0768983096');
-                $user->setImage(null); // ou une URL image random avec $faker->imageUrl()
+                $user->setImage('../Admin.jpeg');
                 $user->setAdmin(1); // Le 1er est admin
+                $user->setActive(1);
+            }elseif ($i == 1) {
+                $user->setPseudo('Camille');
+                $user->setFirstName('Camille');
+                $user->setLastName('Quesnel');
+                $user->setMail('camille@Quesnel.admin');
+                $user->setPhoneNumber('0102030405');
+                $user->setImage('../AdminC.png');
+                $user->setAdmin(1); // Le 2eme aussi est admin
                 $user->setActive(1);
             }
-            if ($i == 1) {
-                $user->setPseudo('User');
+            elseif ($i == 2) {
+                $user->setPseudo('user');
                 $user->setFirstName('User');
                 $user->setLastName('User');
-                $user->setMail('user@user.user');
+                $user->setMail('User@user.user');
                 $user->setPhoneNumber('0102030405');
-                $user->setImage(null); // ou une URL image random avec $faker->imageUrl()
-                $user->setAdmin(1); // Le 1er est admin
+                $user->setImage('../user.jpg');
+                $user->setAdmin(0);
                 $user->setActive(1);
             } else {
-                $user->setPseudo($faker->unique()->userName());
-                $user->setFirstName($faker->firstName());
-                $user->setLastName($faker->lastName());
-                $user->setMail($faker->unique()->email());
-                $user->setPhoneNumber($faker->phoneNumber());
-                $user->setImage(null); // ou une URL image random avec $faker->imageUrl()
-                $user->setAdmin(0); // Le 1er est admin
+                $user->setPseudo($speudo);
+                $user->setFirstName($firstName);
+                $user->setLastName($lastName);
+                $user->setMail($mail);
+                $user->setPhoneNumber($phoneNumber);
+                $user->setImage($image);
+                $user->setAdmin(0);
                 $user->setActive($faker->boolean());
             }
             // Hasher le mot de passe
